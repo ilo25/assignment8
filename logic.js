88d5b0c6e25929d0e5452020523dfd4a3c8b711f
cynthia <cynsellam@gmail.com>
2018-12-09 00:15:50 +0200

    final project

--- logic.js
+++ logic.js
@@ -9,12 +9,11 @@ class App extends React.Component {
         );
     }
 }
-
 class Nav extends React.Component {
     render() {
         return (
-            <div className="degrade">
-                <img id="logo" src="https://image.flaticon.com/icons/svg/951/951971.svg"/>
+            <div className="navbar">
+                <img id="logo" src="https://image.flaticon.com/icons/svg/951/951971.svg" />
                 <h1 className="title">My ToDo list</h1>
             </div>
         )
@@ -25,64 +24,43 @@ class Page extends React.Component {
         super(props)
         this.textInput = "",
             this.state = {
-                arrayItem: [],
-                done: []
+                arrayItem: []
             }
         this.saveItem = this.saveItem.bind(this);
         this.printItem = this.printItem.bind(this);
         this.deleteItem = this.deleteItem.bind(this);
-        // this.printDoneList = this.printDoneList.bind(this);
     }
     saveItem(input) {
         this.textInput = input
     }
-
     printItem() {
         this.state.arrayItem.push(this.textInput.value)
         this.setState({
-            arrayItem: this.state.arrayItem,
+            arrayItem: this.state.arrayItem
         })
         this.textInput.value = ""
     }
-    // printDoneList(event) {
-    //     this.state.done.push(event.target.value)
-    //     this.setState({
-    //         done: this.state.done
-    //     })
-    //     console.log(this.state.done)
-    // }
-    deleteItem(e){
+    deleteItem(e) {
         var item = e.target.parentElement
         item.remove();
     }
     render() {
         var result = this.state.arrayItem.map(
-            x => <Item handleClick={this.printDoneList} handleClick2={this.deleteItem} key={`item${x}`} value={x} />
+            x => <Item handleClick={this.deleteItem} key={`item${x}`} value={x} />
         )
 
-        var done = this.state.done.map(
-            x => <Item key={`item${x}`} value={x} />
-        )
         return (
             <div className="main">
-                <div className="page"> </div>
-                <div className="image">
                 <div className="container">
-                    <input id="text1" type="text" placeholder="Add your task" ref={this.saveItem}  />
-                    <img id="add-btn" onClick={this.printItem} src="https://image.flaticon.com/icons/svg/149/149156.svg"/>
-                    </div>
-                    <ul>{result}</ul>
+                    <input id="text" type="text" placeholder="Add your task" ref={this.saveItem} />
+                    <img id="add-btn" onClick={this.printItem} src="https://image.flaticon.com/icons/svg/149/149156.svg" />
                 </div>
-                <div className="done">
-                    <ul>
-                        {done}
-                    </ul>
-                </div>
-            </div >
+                <ul>{result}</ul>
+                <div className="div"></div>
+            </div>
         )
     }
 }
-
 class Item extends React.Component {
     constructor(props) {
         super(props)
@@ -90,25 +68,21 @@ class Item extends React.Component {
             check: false
         }
         this.checked = this.checked.bind(this)
-    //    this.printDoneList = this.printDoneList.bind(this)
-       this.deleteItem = this.deleteItem.bind(this);
+        this.deleteItem = this.deleteItem.bind(this);
     }
     checked(e) {
         this.setState({
             check: e.target.checked
         })
     }
-//    printDoneList(){
-//        this.props.handleClick(); 
-//    }
-   deleteItem(e){
-    this.props.handleClick2(e); 
-}
+    deleteItem(e) {
+        this.props.handleClick(e);
+    }
     render() {
         return (
-            <div className={this.state.check ? "text" : "todo"}>
+            <div className={this.state.check ? "done" : "todo"}>
 
-                <li key={this.props.key}>{this.props.value}</li><input className="checkbox" onChange={this.checked} onClick={this.printDoneList} type="checkbox" value={this.props.value}></input><img id="trash" onClick={this.deleteItem} src="https://image.flaticon.com/icons/svg/1214/1214926.svg"/>
+                <li key={this.props.key}>{this.props.value}</li><input className="checkbox" onChange={this.checked} onClick={this.printDoneList} type="checkbox" value={this.props.value}></input><img id="trash" onClick={this.deleteItem} src="https://image.flaticon.com/icons/svg/1214/1214926.svg" />
 
             </div>
         )
